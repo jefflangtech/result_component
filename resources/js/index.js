0fd25f3a739e6ce4b61c898ab350976a37afc506
jefflangtech <jeff.m.lang@gmail.com>
2023-02-26 19:09:54 -0800

    Added a toggle button to open and close, slide functionality linked to the buttons, and visibility transitions for what should be viewable in each state

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,32 +1,40 @@
 let openState = false;
+const openButton = document.getElementById('toggle-open-button');
+const closeButton = document.getElementById('toggle-close-button');
 const openSlide = document.getElementById('toggle-slide');
+const resultsScores = document.getElementById('results-scores');
+
+const toggleButtons = function() {
+  openButton.classList.toggle("hidden");
+  closeButton.classList.toggle("hidden");
+}
 
 const toggleSlide = function() {
   if(!openState) {
     document.documentElement.style.setProperty('--translate-X', '-0.15');
     openState = true;
-    console.log("Opened slide");
+    toggleButtons();
+    resultsScores.style.opacity = "1";
   } else if(openState) {
     document.documentElement.style.setProperty('--translate-X', '-0.95');
     openState = false;
-    console.log("Closed slide");
+    setTimeout(toggleButtons, 1000);
+    resultsScores.style.opacity = "0";
   }
 }
 
 async function getData() {
   const response = await fetch('./data.json');
   const data = await response.json();
-  // console.log(data);
 
   data.forEach(element => {
     let arr = [];
     arr = {...element};
     console.log(arr);
   });
-
-
 }
 
 getData();
 
-openSlide.addEventListener("click", toggleSlide, false);
+openButton.addEventListener("click", toggleSlide, false);
+closeButton.addEventListener("click", toggleSlide, false);
