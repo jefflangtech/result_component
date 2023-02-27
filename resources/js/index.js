1a4340d7d01d9bf3d5a573a4259a1c38daba9687
jefflangtech <jeff.m.lang@gmail.com>
2023-02-26 18:29:39 -0800

    Changed body layout to flex and added functionality for JS sliding for both slides and component using custom variables

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,17 @@
-console.log("hey!");
+let openState = false;
+const openSlide = document.getElementById('toggle-slide');
+
+const toggleSlide = function() {
+  if(!openState) {
+    document.documentElement.style.setProperty('--translate-X', '-0.15');
+    openState = true;
+    console.log("Opened slide");
+  } else if(openState) {
+    document.documentElement.style.setProperty('--translate-X', '-0.95');
+    openState = false;
+    console.log("Closed slide");
+  }
+}
 
 async function getData() {
   const response = await fetch('./data.json');
@@ -15,3 +28,5 @@ async function getData() {
 }
 
 getData();
+
+openSlide.addEventListener("click", toggleSlide, false);
